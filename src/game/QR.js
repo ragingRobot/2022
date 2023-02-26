806294e2497977f278d413514fff3e7bc65a23f2
josh milstead <josh.milstead@mailchimp.com>
2023-02-26 17:02:41 -0500

    removed port

--- src/game/QR.js
+++ src/game/QR.js
@@ -4,7 +4,7 @@ export default class QR {
   static getCode() {
     let domain = (new URL(window.location.href));
     const qr = new QRious({
-      value: domain.hostname + ":" + window.location.port + '/controller',
+      value: domain.hostname + (window.location.port ? ":" + window.location.port : "") + '/controller',
     });
     return qr.toDataURL();
   }
