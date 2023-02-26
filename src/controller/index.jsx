9006ecdf76b24323538d7d8316437304cee5b828
josh milstead <josh.milstead@mailchimp.com>
2023-02-26 18:03:24 -0500

    added missing css

--- src/controller/index.jsx
+++ src/controller/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Controller from './Controller';
-
+import './app.css';
 const appRouting = (
   <Router>
     <Routes>
