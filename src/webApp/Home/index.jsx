433154d017b0c2c0dd6b4564ca246f22cbb674e6
Josh Milstead <jmilst20@gmail.com>
2025-07-08 23:31:07 -0400

    copy updates{

--- src/webApp/Home/index.jsx
+++ src/webApp/Home/index.jsx
@@ -4,10 +4,10 @@ import CenteredText from '../CenteredText';
 function Home() {
   return (
     <CenteredText title="Hi There!">
-      <p>I'm a software engineer with an artistic streak, and I'm here to share my love for the intersection of code and creativity with you.
-        I live in the bustling city of Atlanta, GA, where I've made it my mission to bring highly interactive experiences to life. On this site,
-        you'll find a collection of my work that showcases my unique combination of technical skills and creative vision. Whether you're a fellow
-        software engineer or an art lover, I promise you'll find something to admire.</p>
+      <p>I'm a software engineer and artist, I love creating weird games and interactive experiences.
+        I live in Atlanta, GA and sometimes I exhibit my work at local events. On this site,
+        you'll find a collection of my work that showcases my unique combination of technical skills and creativity.</p>
+        <p>This site is also a game, you can play. Use your keyboad, touchscreen controls, or scan the QR code to use your phone as a controller.</p>
     </CenteredText>
   );
 }
